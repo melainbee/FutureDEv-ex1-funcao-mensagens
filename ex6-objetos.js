b51429c63332908ac2b4a8e902251bf8d7599510
Melissa Lâinna <86636206+melainbee@users.noreply.github.com>
2024-03-01 12:26:29 -0300

    Create ex6-objetos.js

---
+++ ex6-objetos.js
@@ -0,0 +1,28 @@
+alunos = [
+  {
+    nome: 'João,
+    idade: 18,
+    nacionalidade: 'Brasileira',
+    profissão: 'estudante',
+  },
+  {
+    nome: 'Natasha,
+    idade: 38,
+    nacionalidade: 'Russa',
+    profissão: 'Diplomata',
+  },
+  {
+    nome: 'Maria,
+    idade: 30,
+    nacionalidade: 'Brasileira',
+    profissão: 'frentista',
+  },
+  {
+    nome: 'Felix,
+    idade: 22,
+    nacionalidade: 'Brasileira',
+    profissão: 'desenvolvedor',
+  }
+]
+console.log( '\nAlunos:' );
+console.log(alunos)
