bac00cd8fa859c720a231e2ea32216e0008565d5
Melissa Lâinna <86636206+melainbee@users.noreply.github.com>
2024-03-01 11:40:15 -0300

    Delete ex4-funcaoparametros.js

--- ex4-funcaoparametros.js
+++
@@ -1,8 +0,0 @@
-\\simplificando o código já indicando a variável
-var resultado = function soma(n1, n2) {
-  
-  return n1 + n2;
-}
-
-\\exemplo de aplicação
-console.log(`A soma de 1 e 2 é igual a ${soma(5, 8)}.`);
